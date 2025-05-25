ea5fd5143090fb3e1db8ab064ad547dd9cd85abd
riflifaizullah <rifligt@gmail.com>
2025-05-25 15:52:23 +0800

    Lab 4

--- lib/base/bottom_nav_bar.dart
+++ lib/base/bottom_nav_bar.dart
@@ -1,67 +1,64 @@
 import 'package:fluentui_icons/fluentui_icons.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:lab_3/home_screen.dart';
+import 'package:ticket_app/controller/bottom_nav_controller.dart';
+import 'package:ticket_app/screens/profile/profile.dart';
+import 'package:ticket_app/screens/search/search_screen.dart';
+import 'package:ticket_app/screens/ticket/ticket_screen.dart';
+import '../screens/home/home_screen.dart';
+import 'package:get/get.dart';
 
-class BottomNavBar extends StatefulWidget {
-  const BottomNavBar({super.key});
+class BottomNavbar extends StatelessWidget {
+  BottomNavbar({super.key});
 
-  @override
-  State<BottomNavBar> createState() => _BottomNavBarState();
-}
+  //dependency injection
+  final BottomNavController controller = Get.put(BottomNavController());
 
-class _BottomNavBarState extends State<BottomNavBar> {
-
-
-  final appScreens = [
+  final List<Widget> appScreens = [
     const HomeScreen(),
-    const Center(child: Text("Search")),
-    const Center(child: Text("Tickets")),
-    const Center(child: Text("Profile")),
+    const SearchScreen(),
+    const TicketScreen(),
+    const ProfileScreen(),
   ];
-  //change our index for bottomnavbar
 
-  int _selectedIndex = 0;
-  void _onItemTapped(int index) {
-    setState((){
-      _selectedIndex = index;
-    });
-  }
+  // change our index for bottomNavBar
+  final int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("My Tickets"),
-      ),
-      body: appScreens[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-       selectedItemColor: Colors.blueGrey,
-       unselectedItemColor: const Color(0xFF526400),
-        showSelectedLabels: false,
-        items: const [
-    BottomNavigationBarItem(
-    icon: Icon(FluentSystemIcons.ic_fluent_home_regular),
-    activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled),
-    label: "Home",
-    ),
-    BottomNavigationBarItem(
-    icon: Icon(FluentSystemIcons.ic_fluent_search_regular),
-    activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled),
-    label: "Search",
-    ),
-    BottomNavigationBarItem(
-    icon: Icon(FluentSystemIcons.ic_fluent_ticket_regular),
-    activeIcon: Icon(FluentSystemIcons.ic_fluent_ticket_filled),
-    label: "Tickets",
-    ),
-    BottomNavigationBarItem(
-    icon: Icon(FluentSystemIcons.ic_fluent_person_regular),
-    activeIcon: Icon(FluentSystemIcons.ic_fluent_person_filled),
-    label: "Profile",
-    ),
-        ],)
-    );
+    print("this works${controller.selectedIndex.value}");
+    return Obx(() {
+      return Scaffold(
+        body: appScreens[controller.selectedIndex.value],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: controller.selectedIndex.value,
+          onTap: controller.onItemTapped,
+          selectedItemColor: Colors.blueGrey,
+          unselectedItemColor: const Color(0xFF526400),
+          showSelectedLabels: false,
+          items: [
+            const BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_home_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled),
+              label: "Home",
+            ),
+            const BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_search_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled),
+              label: "Search",
+            ),
+            const BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_ticket_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_ticket_filled),
+              label: "Tickets",
+            ),
+            const BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_person_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_person_filled),
+              label: "Profile",
+            ),
+          ],
+        ),
+      ); // Or your actual layout
+    });
   }
 }
